b613c52e3e434c829c4f174ed652583fcc3d1be0
Assaf <speko124@gmail.com>
2021-12-23 14:20:57 +0200

    First Version

---
+++ portfolio.py
@@ -0,0 +1,135 @@
+from dataclasses import dataclass
+import seaborn as sns
+import investpy
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+@dataclass
+class StockData:
+    ticker: str
+    p_type: str
+    price: float
+    amount: int
+
+
+class Portfolio(object):
+    def __init__(self):
+        self._assets = dict()
+        self._hist_value = pd.DataFrame()
+
+    @staticmethod
+    def _p_type_multi(p_type):
+        if p_type == "index":
+            return "indices"
+        if p_type[-1] == 's':
+            return p_type
+        return p_type + "s"
+
+    def buy(self, ticker, p_type, amount, price):
+        if ticker in self._assets:
+            asset = self._assets[ticker]
+            self._assets[ticker].price = (asset.amount * asset.price + amount * price) / (asset.amount + amount)
+            self._assets[ticker].amount = asset.amount + amount
+        else:
+            self._assets[ticker] = StockData(ticker=ticker, price=price, amount=amount, p_type=p_type)
+
+    def sell(self, ticker, amount):
+        if ticker in self._assets:
+            asset = self._assets[ticker]
+            self._assets[ticker].amount = asset.amount - amount if (asset.amount - amount) >= 0 else 0
+        else:
+            raise ValueError("No such ticker in assets")
+
+    def _calc_hist_value(self):
+        prices = []
+        for idx, i in enumerate(self._assets):
+            asset = self._assets[i]
+            ticker = asset.ticker
+            product_type = asset.p_type
+            amt = asset.amount
+            search_result = investpy.search_quotes(text=ticker, products=[self._p_type_multi(product_type)],
+                                                   countries=['united states'], n_results=1)
+            raw = search_result.retrieve_recent_data()
+            if idx == 0:
+                dss = raw.index.values
+                ds_parsed = [pd.to_datetime(str(i)).strftime('%Y-%m-%d') for i in dss]
+                res_df = pd.DataFrame(ds_parsed, columns=["ds"])
+            res_df[ticker] = ((raw["Close"] * amt).to_list())
+            prices.append(asset.amount * asset.price)
+        res_df["p_sum"] = res_df.sum(axis=1)
+        res_df["p_buy_sum"] = sum(prices)
+        res_df["gain"] = res_df["p_sum"] - res_df["p_buy_sum"]
+        res_df["gain_ratio"] = (res_df["gain"] * 100) / res_df["p_buy_sum"]
+        self._hist_value = res_df
+
+    def get_hist_value(self):
+        return self._hist_value
+
+    def calc_hist(self):
+        self._calc_hist_value()
+        p_hist = self._hist_value
+        res = [None]
+        for i in range(1, len(p_hist)):
+            curr_sum = p_hist.iloc[i]["p_sum"]
+            prev_sum = p_hist.iloc[i - 1]["p_sum"]
+            chg = round((curr_sum - prev_sum) * 100 / prev_sum, 2)
+            res.append(chg)
+        self._hist_value["p_chg"] = res
+
+    def compare_to(self, ticker, p_type):
+        try:
+            search_result = investpy.search_quotes(text=ticker, products=[self._p_type_multi(p_type)],
+                                                   countries=['united states'], n_results=1)
+        except:
+            raise ValueError(f"missing investing data {ticker} of type {p_type}")
+        if len(self._hist_value) == 0:
+            self.calc_hist()
+        p_hist = self._hist_value[["ds", "p_sum", "p_chg"]].copy()
+        raw = search_result.retrieve_recent_data()
+        p_hist["p_chg_sum"] = p_hist["p_chg"].cumsum()
+        p_hist["t_value"] = raw["Close"].to_list()
+        p_hist["t_chg"] = raw["Change Pct"].to_list()
+        p_hist["t_chg_sum"] = p_hist["t_chg"].cumsum()
+        p_hist["diff"] = p_hist["p_chg"] - p_hist["t_chg"]
+        p_hist["diff_sum"] = p_hist["p_chg_sum"] - p_hist["t_chg_sum"]
+        return p_hist
+
+    def plot_compare_to(self, ticker, p_type):
+        sns.set_theme(style="whitegrid")
+
+        data = self.compare_to(ticker, p_type)[
+            ["ds", "p_chg", "p_chg_sum", "t_chg", "t_chg_sum", "diff", "diff_sum"]]
+        data = data.rename(columns={
+            "p_chg": "protf_chg",
+            "t_chg": f"{ticker}_chg",
+            "p_chg_sum": "protf_chg_sum",
+            "t_chg_sum": f"{ticker}_chg_sum",
+        })
+
+        fig, axes = plt.subplots(2, 2, sharex=True, figsize=(20, 10))
+        fig.suptitle(f'comparing protfolio to {ticker} in the last M')
+        axes[0][0].set_title('Daily Change')
+        axes[0][1].set_title('Accumulated change (from the last M)')
+        axes[1][0].set_title('Daily Chnage Differnce (+ -> protfolio is better)')
+        axes[1][1].set_title('Accumulated Chnage Differnce (+ -> protfolio is better)')
+
+        sns.lineplot(data=data[["protf_chg", f"{ticker}_chg"]], ax=axes[0][0], palette="rocket", linewidth=2.5, )
+        sns.lineplot(data=data[["protf_chg_sum", f"{ticker}_chg_sum"]], ax=axes[0][1], palette="rocket",
+                     linewidth=2.5, )
+        sns.lineplot(data=data[["diff"]], ax=axes[1][0], palette="tab10", linewidth=2.5, )
+        sns.lineplot(data=data[["diff_sum"]], ax=axes[1][1], palette="tab10", linewidth=2.5, )
+        plt.show()
+
+    def plot_hist(self):
+        data = self._hist_value[["ds", "p_sum", "p_chg", "gain_ratio"]].copy()
+
+        fig, axes = plt.subplots(1, 2, sharex=True, figsize=(30, 10))
+        fig.suptitle('Portfolio performance last M')
+        axes[0].set_title('Daily Change')
+        axes[1].set_title('Total Value')
+
+        sns.lineplot(data=data[["p_chg", ]], ax=axes[0], palette="rocket", linewidth=2.5, )
+        sns.lineplot(data=data[["p_sum"]], ax=axes[1], palette="tab10", linewidth=2.5, )
+        plt.show()
+
---
+++ portfolio_ex.py
@@ -0,0 +1,14 @@
+from portfolio import Portfolio
+
+p = Portfolio()
+p.buy("fb", "stock", price=404.5, amount=5)
+p.buy("VTI", "etf", price=240, amount=5)
+
+p.calc_hist()
+print(p.get_hist_value())
+
+print(p.compare_to("S&P 500", "index"))
+
+p.plot_compare_to("S&P 500", "index")
+
+p.plot_hist()
