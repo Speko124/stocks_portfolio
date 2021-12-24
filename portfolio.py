dc87432f36e5a3669f4209ed8b4d0af887855ca4
Assaf <speko124@gmail.com>
2021-12-24 10:13:49 +0200

    Adding recommendation to the data

--- portfolio.py
+++ portfolio.py
@@ -1,8 +1,10 @@
 from dataclasses import dataclass
-import seaborn as sns
+
 import investpy
 import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
+import yfinance as yf
 
 
 @dataclass
@@ -19,14 +21,6 @@ class Portfolio(object):
         self._hist_value = pd.DataFrame()
         self._tickers_hist = dict()
 
-    @staticmethod
-    def _p_type_multi(p_type):
-        if p_type == "index":
-            return "indices"
-        if p_type[-1] == 's':
-            return p_type
-        return p_type + "s"
-
     def buy(self, ticker, p_type, amount, price):
         if ticker in self._assets:
             asset = self._assets[ticker]
@@ -41,6 +35,9 @@ class Portfolio(object):
             self._assets[ticker].amount = asset.amount - amount if (asset.amount - amount) >= 0 else 0
         else:
             raise ValueError("No such ticker in assets")
+
+    def details(self):
+        return pd.DataFrame(self._assets.values())
 
     def _calc_hist_value(self):
         prices = []
@@ -71,6 +68,8 @@ class Portfolio(object):
         self._hist_value = res_df
 
     def get_hist_value(self):
+        if len(self._hist_value) == 0:
+            self.calc_hist()
         return self._hist_value
 
     def calc_hist(self):
@@ -91,7 +90,7 @@ class Portfolio(object):
         try:
             search_result = investpy.search_quotes(text=ticker, products=[self._p_type_multi(p_type)],
                                                    countries=['united states'], n_results=1)
-        except:
+        except (RuntimeError, ConnectionError, ValueError):
             raise ValueError(f"missing investing data {ticker} of type {p_type}")
         if len(self._hist_value) == 0:
             self.calc_hist()
@@ -132,10 +131,12 @@ class Portfolio(object):
         plt.show()
 
     def plot_hist(self):
+        if len(self._hist_value) == 0:
+            self.calc_hist()
         data = self._hist_value[["ds", "p_sum", "p_chg", "gain_ratio"]].copy()
-
+        ds = data["ds"].iloc[-1]
         fig, axes = plt.subplots(1, 2, sharex=True, figsize=(30, 10))
-        fig.suptitle('Portfolio performance last M')
+        fig.suptitle(f'Portfolio performance last M, date - {ds}')
         axes[0].set_title('Daily Change')
         axes[1].set_title('Total Value')
 
@@ -153,3 +154,91 @@ class Portfolio(object):
             row.extend(df_hist.iloc[-1][["Close", "Change Pct", "gain_per_share", "gain_pct"]].to_list())
             res.append(row)
         return pd.DataFrame(res, columns=col_names).sort_values("gain_pct", ascending=False)
+
+    @staticmethod
+    def _p_type_multi(p_type):
+        if p_type == "index":
+            return "indices"
+        if p_type[-1] == 's':
+            return p_type
+        return p_type + "s"
+
+    @staticmethod
+    def _calc_agg_values(signals):
+        pos = {"buy", "less_volatility", "oversold", "strong_buy"}
+        neg = {"sell", "volatile", "overbought", "strong_sell"}
+        neutral = {'neutral', 'hold'}
+        counter = {}
+        for s in signals:
+            if s.lower() in pos:
+                counter["p"] = counter.get("p", 0) + 1
+            elif s.lower() in neg:
+                counter["n"] = counter.get("n", 0) + 1
+            elif s.lower() in neutral:
+                counter["neutral"] = counter.get("natural", 0) + 1
+            else:
+                counter["neutral"] = counter.get("natural", 0) + 1
+        p = counter.get("p", 0)
+        n = counter.get("n", 0)
+        neutral_count = counter.get("neutral", 0)
+        fin_score = p - n
+        if fin_score > 6:
+            return "strong buy"
+        if fin_score > 0:
+            return "buy"
+        if fin_score == 0:
+            return "neutral"
+        if fin_score < -6:
+            return "strong sell"
+        if fin_score < 0:
+            return "sell"
+
+    def _get_technical_indicators(self, ticker, p_type, interval="monthly", ret_type="list"):
+        try:
+            tech_values = investpy.technical_indicators(name=ticker, country='united states', product_type=p_type,
+                                                        interval=interval)
+        except (RuntimeError, ConnectionError, ValueError):
+            search_result = investpy.search_quotes(text=ticker, products=[self._p_type_multi(p_type)],
+                                                   countries=['united states'], n_results=1)
+            tech_values = search_result.retrieve_technical_indicators(interval=interval)
+        names = [f"tech_{interval}"]
+        values = [self._calc_agg_values(tech_values["signal"])]
+        return names, values
+
+    def _get_moving_avg_final(self, ticker, interval="monthly", p_type='stock'):
+        try:
+            ma_values = investpy.moving_averages(name=ticker, country='united states', product_type=p_type,
+                                                 interval=interval)
+        except (RuntimeError, ConnectionError, ValueError):
+            return None
+        ma_list_signal = ma_values['sma_signal'].to_list() + ma_values['ema_signal'].to_list()
+        return self._calc_agg_values(ma_list_signal)
+
+    def _get_investing_tech_data_intervals(self, t, intervals, p_type):
+        res_values = []
+        res_names = []
+        for i in intervals:
+            n, v = self._get_technical_indicators(t, interval=i, ret_type="list", p_type=p_type)
+            res_values += v
+            res_names += n
+            res_values.append(self._get_moving_avg_final(t, i, p_type))
+            res_names.append(f"ma_{i}")
+        return res_names, res_values
+
+    @staticmethod
+    def _gen_yf_recommendation(ticker):
+        try:
+            return yf.Ticker(ticker.upper()).info.get("recommendationKey")
+        except (RuntimeError, ConnectionError, ValueError):
+            return None
+
+    def gen_recommendations(self):
+        names = []
+        res = []
+        for a in self._assets.values():
+            n, v = self._get_investing_tech_data_intervals(t=a.ticker, intervals=["daily", "weekly", "monthly"],
+                                                           p_type=a.p_type)
+            names = ["ticker"] + ["yf"] + n
+            v = [a.ticker.upper()] + [self._gen_yf_recommendation(a.ticker)] + v
+            res.append(v)
+        return pd.DataFrame(res, columns=names)
